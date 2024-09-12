daf28c44a4089cd3508ff93cbe709125765572d9
Jabbar Shaikh <101566891+jabbar024@users.noreply.github.com>
2024-09-12 14:36:44 +0530

    Time: 44 ms (39.75%), Space: 44.9 MB (32.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -12,9 +12,5 @@ class Solution {
         return null;
         
     }
-    public static void main(String[] args){
-        int nums[] = {2, 7, 11, 15};
-        int target = 9;
-        twoSum(nums, target);
-    }
+    
 }
