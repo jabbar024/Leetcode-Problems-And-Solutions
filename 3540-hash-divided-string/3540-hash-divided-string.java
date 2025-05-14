3ef18aa8102714e969350c51a2444594fa09654f
Jabbar Shaikh <101566891+jabbar024@users.noreply.github.com>
2025-05-14 21:55:40 +0530

    Time: 2 ms (92.9%), Space: 44.6 MB (94.46%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.java
+++ 3540-hash-divided-string/3540-hash-divided-string.java
@@ -2,16 +2,16 @@ class Solution
 {
     public String stringHash(String s, int k) 
     {
-        char alphabate[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',} ;
+        String alphabate[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"} ;
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for(int i=0; i<s.length(); )
         {
-            result += alphabate[getDividedString(s.substring(i, i+k))] ;
+            result.append( alphabate[getDividedString(s.substring(i, i+k))] ) ; 
             i+=k ;          
         }
 
-        return result ;
+        return result.toString();
         
     }
 
